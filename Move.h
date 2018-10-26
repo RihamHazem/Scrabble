27ae6855b0b9704c9213d552c5b5d35a5ead73a6
omniasaleh <engomnia2019@gmail.com>
2018-10-26 16:09:41 +0200

    Add files via upload

---
+++ Move.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+#include<map>
+#include<vector>
+#include<string>
+#include <sstream>
+using namespace std;
+struct Move {
+	string word;
+	int x, y;
+	int direction;
+};
